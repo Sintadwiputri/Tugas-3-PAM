cbc9033463c387b024695df0f65802eb2f6b3a4a
Sintadwiputri <104444171+Sintadwiputri@users.noreply.github.com>
2023-03-19 09:52:30 +0700

    Add files via upload

---
+++ component/data.js
@@ -0,0 +1,48 @@
+const DataPenerbangan = [
+  [
+    "Bandung",
+    "Lampung",
+    "Jawa",
+    "Bali",
+    "Aceh",
+    "Jakarta",
+    "Kalimantan",
+    "Maluku",
+    "Banten",
+    "Palembang",
+  ],
+  [
+    "Lampung",
+    "Bandung",
+    "Palembang",
+    "Jawa",
+    "Kalimantan",
+    "Maluku",
+    "Banten",
+    "Jakarta",
+    "Bali",
+    "Aceh",
+  ],
+  [
+    "20 April 2023",
+    "1 Januari 2023",
+    "18 Januari 2023",
+    "20 April 2023",
+    "21 Januari 2023",
+    "20 April 2023",
+  ],
+  [
+    "Elang Air",
+    "Wings Air",
+    "Majapahit Air",
+    "Elang Air",
+    "Wings Air",
+    "Majapahit Air",
+    "Wings Air",
+    "Elang Air",
+    "Wings Air",
+    "Majapahit Air",
+  ],
+];
+
+export default DataPenerbangan;
